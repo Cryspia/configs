4b27f034895c2eb200edfa1f9ed4e98c034a9c09
Cocabell the Virtues <kaiyuanl@gmail.com>
2014-09-25 07:10:45 +0800

    Add a cpp framework

---
+++ framework/cppframe.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <utility>
+using namespace std;
+
+int main()
+{
+	return 0;
+}
